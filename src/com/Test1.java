7352e7df70d80122dd4983caa129d0325d764ae7
mayuri25091994 <mayurilonare1@gmail.com>
2024-07-31 18:15:51 +0530

    second commit

--- src/com/Test1.java
+++ src/com/Test1.java
@@ -4,6 +4,9 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("TestCase-1");
+		System.out.println("TetCase-2");
+		System.out.println("TetCase-3");
+		
 
 	}
 
